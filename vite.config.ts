a0c1ce12e93be3e0f54b2ac361aacbab5f50ed93
Abdullah <mabdullah@techtiz.co>
2025-02-04 13:42:03 +0500

    add headers in vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import dotenv from 'dotenv';
 import path from 'path';
+
 // Load environment variables
 dotenv.config();
+
 export default defineConfig({
   define: {
     'process.env': process.env, // Make environment variables accessible in the client code
-  plugins: [react(),    svgr(),],
+  },
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'), // Add the alias for src
     },
   },
-}
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; object-src 'none'; sandbox allow-scripts allow-same-origin",
+    },
+  },
 });
